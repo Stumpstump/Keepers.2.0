1d79d452091e23b2d1e5086bd506e89aad22452f
Raymond <stump865@gmail.com>
2018-06-25 23:13:16 -0500

    Trigger Testing

---
+++ Keepers.-master/Assets/Scripts/InteractionTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionTrigger : MonoBehaviour
+{
+
+    public GameObject light;
+    private bool on = false;
+
+
+    // Use this for initialization
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown("joystick button 3") && !on)
+        {
+            light.SetActive(true);
+            on = true;
+        }
+        else if (Input.GetKeyDown("joystick button 3") && on)
+        {
+            light.SetActive(false);
+            on = false;
+        }
+    }
+}

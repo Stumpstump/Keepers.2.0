70d9b20066094ffc3f6e68e1610ee5ff2bd362eb
thatMarcus <marcushendd@gmail.com>
2018-06-25 23:10:05 -0500

    Camera Update

---
+++ Keepers.-master/Assets/CameraReturn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraReturn : MonoBehaviour {
+
+    public Camera Camera;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown("joystick button 5"))
+        {
+          
+
+        }
+
+
+
+    }
+}

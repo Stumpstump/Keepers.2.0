ca1d573718ea8c7591738c8a3877b8a5a4026c63
thatMarcus <marcushendd@gmail.com>
2018-06-27 05:46:33 -0500

    Perfect camera, pathing setup, spirit in Snow scene

---
+++ Keepers.-master/Assets/DestroyGhost.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyGhost : MonoBehaviour {
+
+    public GameObject avatar1;
+
+    void OnCollisionEnter(Collision Ghost)
+    {
+        if (Ghost.gameObject.name == "Spirit")
+        {
+            avatar1.gameObject.SetActive(false);
+        }
+    }
+}

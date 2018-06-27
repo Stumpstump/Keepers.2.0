91a24d5d3c9c80ffd5adf0122a42252fd88180b2
thatMarcus <marcushendd@gmail.com>
2018-06-27 12:52:41 -0500

    Shadow particle and shadow interact script

---
+++ Keepers.-master/Assets/BTriggerPathFollow.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BTriggerPathFollow : MonoBehaviour {
+
+    public Transform[] path;
+    public float speed = 5.0f;
+    public float reachDistance = 1.0f;
+    public int currentPoint = 0;
+    bool CheckForBark = false;
+
+    private void OnTriggerEnter(Collider Player)
+    {
+        CheckForBark = true;
+    }
+    private void OnTriggerExit(Collider Player)
+    {
+        CheckForBark = false;
+    }
+ 
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.M) || Input.GetKeyDown("joystick button 1") && CheckForBark)
+
+            {
+                float dist = Vector3.Distance(path[currentPoint].position, transform.position);
+                transform.position = Vector3.MoveTowards(transform.position, path[currentPoint].position, Time.deltaTime * speed);
+
+                if (dist <= reachDistance)
+                {
+                    currentPoint++;
+                }
+            }
+
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (path.Length > 0)
+            {
+                for (int i = 0; i < path.Length; i++)
+                {
+                    if (path[i] != null)
+                    {
+                        Gizmos.DrawSphere(path[i].position, reachDistance);
+                    }
+                }
+            }
+        }
+    }

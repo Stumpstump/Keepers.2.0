1eebb5a33d03922ae40c505cc16435aa6defa5d0
thatMarcus <marcushendd@gmail.com>
2018-06-27 22:43:38 -0500

    Tundra jumping puzzle

---
+++ Keepers.-master/Assets/DestoryShadow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestoryShadow : MonoBehaviour {
+
+    public GameObject TheShadow;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	
+	void OnTriggerStay(Collider Shadow)
+    {
+
+        Destroy(Shadow.gameObject);
+
+		
+	}
+}
---
+++ Keepers.-master/Assets/JumpParticle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpParticle : MonoBehaviour {
+
+    public ParticleSystem particles;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown("joystick button 0"))
+
+        {
+            particles.Play();
+        }
+
+    }
+}
